89efe75fa846bd561c1dd3b7af7374aa1b188038
Monge-Bayir <ojdupool2004@mail.ru>
2026-03-02 14:23:33 +0300

    проверка данных после transform

--- airflow/dags/cbr_pipeline_taskflow.py
+++ airflow/dags/cbr_pipeline_taskflow.py
@@ -1,6 +1,12 @@
 from datetime import datetime
+from time import clock_settime_ns
+
 from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
+import csv
+
+from sqlalchemy.testing import rowset
+
 
 @dag(
     dag_id='pipeline_taskflow',
@@ -31,6 +37,21 @@ def cbr_pipeline_taskflow():
         return processed_path
 
     @task(retries=2)
+    def check_data_from_transform(processed_path: str):
+        with open(processed_path, newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+
+        if len(rows) <= 50:
+            raise ValueError(f'Слишком мало данных, их {rows}')
+
+        for row in rows:
+            if float(row["value_per_1"]) <= 0:
+                raise ValueError("value_per_1 <= 0")
+
+        return processed_path
+
+    @task(retries=2)
     def analytics(processed_path: str) -> None:
         from src.analytics import analytics_check
         ds = get_current_context()["ds"]
@@ -40,6 +61,7 @@ def cbr_pipeline_taskflow():
 
     raw_path = extract()
     processed_path = transform(raw_path)
-    analytics(processed_path)
+    checked_path = check_data_from_transform(processed_path)
+    analytics(checked_path)
 
 cbr_pipeline_taskflow()
